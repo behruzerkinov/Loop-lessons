bac186c7efe2e9b49b589f17894fd51e065fcdb1
Behruz Erkinov <erkinovbehruz0@gmail.com>
2022-08-26 22:44:33 -0700

    test-2

---
+++ app.js
@@ -0,0 +1,54 @@
+// for (let i = 1; i <= 20; i += 2) {
+//           console.log(i);
+// }
+
+// let laptop = ['DELL', 800, 'black', 8]
+
+// for (let i = 0; i < laptop.length; i++) {
+//           console.log(laptop[i]);
+// }
+
+// let j = 0
+
+// while (j < laptop.length) {
+//           console.log(laptop[j])
+//           j++
+// }
+
+// let number;
+
+// function fizzBuzz(number) {
+//   if (number % 3 === 0 && number % 5 !== 0) {
+//     console.log("fizz");
+//   } else if (number % 5 === 0 && number % 3 !== 0) {
+//     console.log("buzz");
+//   } else if (number % 3 === 0 && number % 5 === 0) {
+//     console.log("fizzBuzz");
+//   } else if (typeof number !== "number") {
+//     console.log("Bu son emas");
+//   } else {
+//     console.log("Bu son 3 ga ham 5 ga ham bo'linmaydi");
+//   }
+// }
+
+// fizzBuzz(10);
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+let aSmile, bSmile
+
+function monkeyTrouble(aSmile, bSmile) {
+  if (aSmile && bSmile) {
+    console.log(true);
+  } else if (!aSmile && !bSmile) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+monkeyTrouble(aSmile, bSmile);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
